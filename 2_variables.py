937495fa5ba50edec03e334f26625efb360f0db9
Iness <inesshaddach@gmail.com>
2024-08-16 20:38:45 +0100

    2_variables

---
+++ 2_variables.py
@@ -0,0 +1,10 @@
+name = "Yahya" #string
+age = 28 #int
+money = 45.50 #float
+happy = True #boolean (true,false)
+
+print("name: " + name)
+print("age: " + str(age))
+print("money: " + str(money))
+print("happy: " + str(happy))
+
